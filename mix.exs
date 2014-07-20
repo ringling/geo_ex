002c5a69622796263ec9842914b807ae04da8f3d
Thomas Ringling <thomas.ringling@gmail.com>
2014-07-20 16:21:49 +0200

    Remove mix.exs deps example comments

--- mix.exs
+++ mix.exs
@@ -15,20 +15,10 @@ defmodule GeoEx.Mixfile do
     [applications: []]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:apex, "~>0.3.0"},
       {:exprintf, github: "parroty/exprintf"}
-
     ]
 
   end
